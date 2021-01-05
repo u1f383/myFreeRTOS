ce334d10ff067d4ad02a1c63faf8fdc54205658c
u1f383 <cc85nod@gmail.com>
2021-01-05 23:11:24 +0800

    init

---
+++ Demo/CORTEX_LM3S811_GCC/gcc/integer.d
@@ -0,0 +1,17 @@
+gcc/integer.o: ../Common/Minimal/integer.c /usr/include/newlib/stdlib.h \
+ /usr/include/newlib/machine/ieeefp.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/stdlib.h /usr/include/newlib/alloca.h \
+ ../../Source/include/FreeRTOS.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h FreeRTOSConfig.h \
+ ../../Source/include/projdefs.h ../../Source/include/portable.h \
+ ../../Source/include/deprecated_definitions.h \
+ ../../Source/include/../../Source/portable/GCC/ARM_CM3/portmacro.h \
+ ../../Source/include/mpu_wrappers.h ../../Source/include/task.h \
+ ../../Source/include/list.h ../Common/include/integer.h
---
+++ Demo/CORTEX_LM3S811_GCC/gcc/port.d
@@ -0,0 +1,9 @@
+gcc/port.o: ../../Source/portable/GCC/ARM_CM3/port.c \
+ ../../Source/include/FreeRTOS.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h FreeRTOSConfig.h \
+ ../../Source/include/projdefs.h ../../Source/include/portable.h \
+ ../../Source/include/deprecated_definitions.h \
+ ../../Source/include/../../Source/portable/GCC/ARM_CM3/portmacro.h \
+ ../../Source/include/mpu_wrappers.h ../../Source/include/task.h \
+ ../../Source/include/list.h
